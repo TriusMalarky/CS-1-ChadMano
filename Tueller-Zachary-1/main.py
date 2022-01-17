0fe634e6b97a8be6c0064b98d8cb6620869c1bfa
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-01-17 10:57:39 -0700

    Init-1-1
    Initial commit for assignment 1

---
+++ Tueller-Zachary-1/main.py
@@ -0,0 +1,11 @@
+# Zachary Tueller
+# CS1400 - MO1
+# Assignment 1
+import task1
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
---
+++ Tueller-Zachary-1/task1.py
@@ -0,0 +1,4 @@
+# Zachary Tueller  
+# CS1400 - MO1
+# Assignment 1
+
